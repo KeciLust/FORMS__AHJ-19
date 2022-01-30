d04235894f6718ea79029038852d8ee35688c03f
Denis Nikitin <dinya.nikitin@gmail.com>
2022-01-30 21:38:29 +0300

    change e2e

--- src/e2e/e2e.test.js
+++ src/e2e/e2e.test.js
@@ -1,11 +1,22 @@
 import puppeteer from 'puppeteer';
+import { fork } from 'child_process';
 
 jest.setTimeout(30000);
 describe('Widget', () => {
   let browser = null;
   let page = null;
+  let server = null;
   const baseUrl = 'http://localhost:9000';
   beforeAll(async () => {
+    server = fork(`${__dirname}/e2e.server.js`);
+    await new Promise((resolve, reject) => {
+      server.on('error', reject);
+      server.on('message', (message) => {
+        if (message === 'ok') {
+          resolve();
+        }
+      });
+    });
     browser = await puppeteer.launch({
       headless: false,
       slowMo: 100,
@@ -15,13 +26,12 @@ describe('Widget', () => {
   });
   afterAll(async () => {
     await browser.close();
+    server.kill();
   });
   test('test widget', async () => {
     await page.goto(baseUrl);
     const widget = await page.$('.widget_run');
     widget.click();
     await page.waitForSelector('.widget_form');
-    await browser.close();
   });
-
 });
